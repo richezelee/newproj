cb806cee32673c37b7c3649d514744f8255635fb
Ezelee <ezelee@ezelees-mbp.myfiosgateway.com>
2021-12-12 20:33:39 -0500

    terraform script with SGs

---
+++ bastionserver.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = "${var.ami}"
+  key_name                    = "${var.keypair}"
+  instance_type               = "t2.micro"
+  security_groups             = ["${aws_security_group.bastion-sg.name}"]
+  associate_public_ip_address = true
+}
+
+resource "aws_security_group" "bastion-sg" {
+  name   = "aws_security_group.baston-sg"
+  vpc_id = "${var.vpc}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0 
+    to_port     = 0 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
